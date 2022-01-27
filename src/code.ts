14bd2e6ef7cdd4c177d9f1b0aed15ecc70e9e655
Graham Sysko <gsysko@gmail.com>
2022-01-27 21:30:48 +0000

    Add comments

--- src/code.ts
+++ src/code.ts
@@ -1,10 +1,13 @@
-
+//Get current selection
 const selection = figma.currentPage.selection
+//If anything is selected...
 if (selection.length) {
+  //...initialize width/height to first items width and height
   var width = selection[0].width
   var height = selection[0].height
 
   selection.forEach(selectItem => {
+    //Then check if subsequent widths/heights match, otherwise set to null (meaning it is a mixed selection).
     if (selectItem.width != width){
       width = null
     }
@@ -14,22 +17,31 @@ if (selection.length) {
   })
 }
 
+//On quick action triggering...
 figma.parameters.on('input', ({ parameters, key, query, result }: ParameterInputEvent) => {
   switch (key) {
+    //For width parameter...
     case 'width':
       const widths = [width + "l"]
+      //If not a mixed selection...
       if (width) {
+        //Suggest current width
         result.setSuggestions(widths.filter(s => s.includes(query)))
       } else {
-        result.setSuggestions(["Mixed"])
+        //Else, suggest "Mixed"
+        result.setSuggestions(["Mixed"].filter(s => s.includes(query)))
       }
       break
+    //For height parameter...
     case 'height':
       const heights = [height + "t"]
+      //If not a mixed selection...
       if (height) {
+        //Suggest current height
         result.setSuggestions(heights.filter(s => s.includes(query)))
       } else {
-        result.setSuggestions(["Mixed"])
+        //Else, suggest "Mixed"
+        result.setSuggestions(["Mixed"].filter(s => s.includes(query)))
       }
       break
     default:
@@ -37,34 +49,47 @@ figma.parameters.on('input', ({ parameters, key, query, result }: ParameterInput
   }
 })
 
+//On plugin begins running...
 figma.on('run', ({ command, parameters }: RunEvent) => {
+  //If nothing selected...
   if (selection.length < 1) {
+    //Prompt user to  select something.
     figma.closePlugin("Select at least one item")
     return
   }
   switch (command) {
+    //Defaul command
     case "":
+      //If command comes from quick action...
       if (parameters) {
+        //Convert from string -> number
         let newWidth = parseInt(parameters.width, 10)
+        //Initialize a horizontal resize direction
         let horizontalDirection = "l"
+        //Change the direction if a different letter is found
         if (parameters.width.search(/\d*c/i) == 0) {
           horizontalDirection = "c"
         } else if (parameters.width.search(/\d*r/i) == 0) {
           horizontalDirection = "r"
         }
-
+        //Convert from string -> number
         let newHeight = parseInt(parameters.height, 10)
+        //Initialize a verical resize direction
         let verticalDirection = "t"
+        //Change the direction if a different letter is found
         if (parameters.height.search(/\d*c/i) == 0) {
           verticalDirection = "c"
         } else if (parameters.height.search(/\d*b/i) == 0) {
           verticalDirection = "b"
         }
 
+        //Do the resize
         resize( newWidth, horizontalDirection, newHeight, verticalDirection )
       } else {
+        //Else, command comes from plugin menu; launch plugin...
         figma.showUI(__html__)
         figma.ui.resize(300, 144)
+        //And populate with the appropriate size values.
         figma.ui.postMessage({ type: 'setSize' , width: width, height: height })
         break
       }
@@ -72,16 +97,20 @@ figma.on('run', ({ command, parameters }: RunEvent) => {
 })
 
 figma.ui.onmessage = msg => {
+  //Receive message from plugin UI...
   if (msg.type === 'resize') {
+    //Pass on the values and do the resize
     resize(msg.width, msg.horizontalDirection, msg.height, msg.verticalDirection)
   }
 }
 
-//TODO need to account for when height/width is "Mixed" (or other NaN)
 function resize(width: number, horizontalDirection: string, height: number, verticalDirection: string) {
+  //For each item selected...
   selection.forEach(selected => {
+    //Record the amount each dimension grows/shrinks.
     let deltaH = selected.width - width
     let deltaV = selected.height - height
+    //For all resizable node types...
     switch(selected.type){
       case "BOOLEAN_OPERATION" :
       case "COMPONENT" :
@@ -99,24 +128,36 @@ function resize(width: number, horizontalDirection: string, height: number, vert
       case "STAR" :
       case "TEXT" :
       case "VECTOR":
+        //If width is a number (i.e. not NaN because it is "Mixed")...
         if (width) {
+          //Resize its width
           selected.resize(width, selected.height)
           switch(horizontalDirection){
+            //If it should remain centered...
             case "c":
+              //Shift its position to offset the width change.
               selected.x = selected.x + deltaH/2
               break
+            //If it should remain right aligned...
             case "r":
+              //Shift its position to offset the width change.
               selected.x = selected.x + deltaH
               break
           }
         }
+        //If height is a number (i.e. not NaN because it is "Mixed")...
         if (height) {
+          //Resize its height
           selected.resize(selected.width, height)
           switch(verticalDirection){
+            //If it should remain centered...
             case "c":
+              //Shift its position to offset the height change.
               selected.y = selected.y + deltaV/2
               break
+            //If it should remain bottom aligned...
             case "b":
+              //Shift its position to offset the height change.
               selected.y = selected.y + deltaV
               break
           }
@@ -124,5 +165,6 @@ function resize(width: number, horizontalDirection: string, height: number, vert
         break
     }
   })
+  //Phew! We are done.
   figma.closePlugin()
 }
--- src/ui.ts
+++ src/ui.ts
@@ -1,6 +1,7 @@
 import 'figma-plugin-ds/dist/figma-plugin-ds.css'
 import './ui.css'
 
+//Focus the first input. Select text in inputs when focused, and submit on "Enter" key.
 let inputwidth = document.getElementById("input_width") as HTMLInputElement
 inputwidth.onfocus = event => inputwidth.select()
 inputwidth.onkeydown = event => {if(event.key == "Enter")resize()}
@@ -8,6 +9,7 @@ let inputheight = document.getElementById("input_height") as HTMLInputElement
 inputheight.onfocus = event => inputheight.select()
 inputheight.onkeydown = event => {if(event.key == "Enter")resize()}
 
+//Set click listeners on all the buttons
 document.getElementById('button_resize').onclick = () => resize()
 document.getElementById('button_w_l').onclick = () => setHorizontalAnchor("l")
 document.getElementById('button_w_c').onclick = () => setHorizontalAnchor("c")
