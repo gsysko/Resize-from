0f4222872301f10f3e38920a95bf7946eb8fcf55
Graham Sysko <gsysko@gmail.com>
2022-02-26 15:11:08 +0000

    Closes #11

--- src/ui.ts
+++ src/ui.ts
@@ -7,19 +7,27 @@ const reLetters = /[A-Za-z]/g
 //Focus the first input. Select text in inputs when focused, and submit on "Enter" key.
 let inputwidth = document.getElementById("input_width") as HTMLInputElement
 inputwidth.onfocus = event => inputwidth.select()
+inputwidth.oninput = () => checkSelection();
 inputwidth.onkeydown = event => {if(event.key == "Enter")resize()}
 let inputheight = document.getElementById("input_height") as HTMLInputElement
 inputheight.onfocus = event => inputheight.select()
+inputheight.oninput = () => checkSelection();
 inputheight.onkeydown = event => {if(event.key == "Enter")resize()}
 
 //Set click listeners on all the buttons
 document.getElementById('button_resize').onclick = () => resize()
-document.getElementById('button_w_l').onclick = () => setHorizontalAnchor("l")
-document.getElementById('button_w_c').onclick = () => setHorizontalAnchor("c")
-document.getElementById('button_w_r').onclick = () => setHorizontalAnchor("r")
-document.getElementById('button_h_t').onclick = () => setVerticalAnchor("t")
-document.getElementById('button_h_c').onclick = () => setVerticalAnchor("c")
-document.getElementById('button_h_b').onclick = () => setVerticalAnchor("b")
+let buttonWL = document.getElementById('button_w_l')
+buttonWL.onclick = () => setHorizontalAnchor("l")
+let buttonWC = document.getElementById('button_w_c')
+buttonWC.onclick = () => setHorizontalAnchor("c")
+let buttonWR = document.getElementById('button_w_r')
+buttonWR.onclick = () => setHorizontalAnchor("r")
+let buttonHT = document.getElementById('button_h_t')
+buttonHT.onclick = () => setVerticalAnchor("t")
+let buttonHC = document.getElementById('button_h_c')
+buttonHC.onclick = () => setVerticalAnchor("c")
+let buttonHB = document.getElementById('button_h_b')
+buttonHB.onclick = () => setVerticalAnchor("b")
 
 //On receiving an event from code.ts...
 window.onmessage = async (event) => {
@@ -30,6 +38,29 @@ window.onmessage = async (event) => {
     widthTextbox.select()
     const heightTextbox = document.getElementById('input_height') as HTMLInputElement
     heightTextbox.value = event.data.pluginMessage.height ? event.data.pluginMessage.height : "Mixed"
+  }
+}
+
+function checkSelection() {
+  buttonWL.className = "icon-button"
+  buttonWC.className = "icon-button"
+  buttonWR.className = "icon-button"
+  buttonHT.className = "icon-button"
+  buttonHC.className = "icon-button"
+  buttonHB.className = "icon-button"
+  if (inputwidth.value.search(/\d*c/i) != -1) {
+    buttonWC.className = "icon-button icon-button--selected"
+  } else if (inputwidth.value.search(/\d*r/i) != -1) {
+    buttonWR.className = "icon-button icon-button--selected"
+  } else {
+    buttonWL.className = "icon-button icon-button--selected"
+  }
+  if (inputheight.value.search(/\d*c/i) != -1) {
+    buttonHC.className = "icon-button icon-button--selected"
+  } else if (inputheight.value.search(/\d*b/i) != -1) {
+    buttonHB.className = "icon-button icon-button--selected"
+  } else {
+    buttonHT.className = "icon-button icon-button--selected"
   }
 }
 
@@ -65,8 +96,10 @@ function resize() {
 
 function setHorizontalAnchor(anchorPoint: string) {
   inputwidth.value = inputwidth.value.replaceAll(reLetters, "") + anchorPoint
+  checkSelection()
 }
 
 function setVerticalAnchor(anchorPoint: string) {
   inputheight.value = inputheight.value.replaceAll(reLetters, "") + anchorPoint
+  checkSelection()
 }
